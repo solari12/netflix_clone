d74ab7bcf36f67a89ae67fd6fda3fa762943454d
solari12 <vantuan11102004@gmail.com>
2025-03-15 11:05:03 +0700

    Create component and develop navbar

---
+++ src/components/TitlesCards/TitlesCards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import'./TittlesCards.css'
+
+const TitlesCards = () => {
+  return (
+    <div className='titlescards'>
+      
+    </div>
+  )
+}
+
+export default TitlesCards
